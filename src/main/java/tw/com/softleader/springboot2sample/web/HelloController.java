b1a8e6203b0e5161e564dab0b1e7bb467cb4321a
Gary Lee <jakarta99@gmail.com>
2018-03-06 23:36:51 +0800

    add application.properties to set view.prefix and suffix.

--- src/main/java/tw/com/softleader/springboot2sample/web/HelloController.java
+++ src/main/java/tw/com/softleader/springboot2sample/web/HelloController.java
@@ -2,14 +2,13 @@ package tw.com.softleader.springboot2sample.web;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
 	
 	@RequestMapping("/")
 	public String index() {
-		return "/WEB-INF/pages/index.jsp";
+		return "/index";
 	}
 
 }
